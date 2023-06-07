392fe4a6cec550d62acca3f99350b40f313e468c
fouad-abdelhady <32526345+fouad-abdelhady@users.noreply.github.com>
2023-06-07 18:01:30 +0300

    Update apis.js

--- js/data/services/apis.js
+++ js/data/services/apis.js
@@ -4,13 +4,16 @@ class Apis{
     recent;
     products;
     orders;
-
+    login;
+    register;
     constructor(){
         this.categories = "categories/";
         this.feactured = "products/getFeatured";
         this.recent = "products/getRecent";
         this.products = "products/";
         this.orders = "orders/";
+        this.login = "users/login";
+        this.register = "users/register";
     }
     
 }
