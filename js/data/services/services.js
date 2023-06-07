02dacb9c80d594a60f922c82e921046a59d9bdbe
Fouad Abdelhady <fouad.abd-elhady@outlook.com>
2023-06-07 18:21:35 +0200

    added: documentation to the Services class

--- js/data/services/services.js
+++ js/data/services/services.js
@@ -1,18 +1,37 @@
 class Services {
     #token;
     #baseUrl;
+    /**
+     * 
+     * @param {userToken} token this is the authentication token which will be send in the request header 
+     */
     constructor(token){
         this.#token = token;
         this.#baseUrl = "http://localhost:5000/api/"
     }
+    /**
+     * 
+     * @param {string} baseUrl it is the common part in all apis 
+     * @example api1 = "http://localhost:5000/api/users/register", api2 = "http://localhost:5000/api/products/", baseUrl = "http://localhost:5000/api/"
+     */
     setBaseUrl(baseUrl){
         this.#baseUrl = baseUrl;
     }
+
+     /**
+     * 
+     * @param {userToken} token this is the authentication token which will be send in the request header 
+     */
     setToken(token){
         this.#token = token;
     }
 
-
+    /**
+     * 
+     * @param {string} api value from the class Apis represent the dirctory which will be added to the base url 
+     * @param {object} body key value object to be send in the request body
+     * @returns {json}
+     */
     async postRequest(api, body){
         let requestBody = body??{};
         const response = await fetch(`${this.#baseUrl}${api}`, {
@@ -26,7 +45,12 @@ class Services {
     const data = await response.json()
     return data;
     }
-
+    /**
+     * 
+     * @param {string} api value from the class Apis represent the dirctory which will be added to the base url 
+     * @param {object} params key value object to be send in the get request url
+     * @returns {json}
+     */
     async getRequest(api, params){
         let url = `${this.#baseUrl}${api}`;
         if(params != null){
@@ -44,23 +68,3 @@ class Services {
         return data;
     }
 }
-
-async function  getData() {
-    let services = new Services();
-    let param = {'q':'phone'}
-    let response  = await services.getRequest("products/search", param);
-
-   response.products.forEach(element => {
-        console.log(element.title);
-        element.images.forEach(item =>{
-            console.log(item);
-        });
-    });
-}
-
-async function  getDataViaPost() {
-    let services = new Services();
-    let response  = await services.postRequest("products/add", {title: "Mercides"});
-    console.log(response);
-}
-getData();
