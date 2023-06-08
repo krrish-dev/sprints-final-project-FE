8e50fcbe085a274a34ab8e0c920a6fc1e0135a08
krrish_dev <justbegintoday@gmail.com>
2023-06-08 14:49:29 +0300

    Update: The categories have been sorted by product count in descending order.

--- js/home.js
+++ js/home.js
@@ -64,7 +64,7 @@ class CategoryRenderer {
     if (targetElement) {
       targetElement.innerHTML = ""; // Clear any existing content
 
-      categories.forEach((category) => {
+      categories.slice(0, 4).forEach((category) => {
         const { _id, name, image, productCount } = category;
         const categoryObj = new Category(_id, name, image, productCount);
         const categoryHtml = categoryObj.renderBox();
@@ -77,11 +77,12 @@ class CategoryRenderer {
     const categoriesMenu = await this.fetchCategories();
     this.renderCategories(categoriesMenu);
   }
+
   async initBoxCategories() {
     const categories = await this.fetchCategories();
-    this.renderBoxCategories(categories);
+    const sortedCategories = categories.sort((a, b) => b.productCount - a.productCount);
+    this.renderBoxCategories(sortedCategories);
   }
- 
 }
 
 // Usage
@@ -92,89 +93,5 @@ const categoryRenderer = new CategoryRenderer("http://localhost:5000/api/categor
 categoryRenderer.initBoxCategories();
 
 
-
-// another same code result 
-// class Category {
-//   constructor(id, name, image, productCount) {
-//     this.id = id;
-//     this.name = name;
-//     this.image = image;
-//     this.productCount = productCount;
-//   }
-// }
-
-// class BaseCategoryRenderer {
-//   constructor(apiUrl, targetElementId) {
-//     this.apiUrl = apiUrl;
-//     this.targetElementId = targetElementId;
-//   }
-
-//   async fetchCategories() {
-//     try {
-//       const response = await fetch(this.apiUrl);
-//       const data = await response.json();
-//       return data.data;
-//     } catch (error) {
-//       console.error("Error fetching categories:", error);
-//       return [];
-//     }
-//   }
-
-//   async init() {
-//     const categories = await this.fetchCategories();
-//     this.render(categories);
-//   }
-// }
-
-// class CategoryMenuRenderer extends BaseCategoryRenderer {
-//   render(categories) {
-//     const linkList = categories.map((category) => {
-//       const { _id, name } = category;
-//       const categoryObj = new Category(_id, name);
-//       return `<a data-id="${categoryObj.id}" onclick="getCities(${categoryObj.id})" href="products.php?cat_id=${categoryObj.id}" class="nav-item nav-link">${categoryObj.name}</a>`;
-//     }).join("");
-
-//     const targetElement = document.getElementById(this.targetElementId);
-//     if (targetElement) {
-//       targetElement.innerHTML = linkList;
-//     }
-//   }
-// }
-
-// class CategoryBoxRenderer extends BaseCategoryRenderer {
-//   render(categories) {
-//     const targetElement = document.getElementById(this.targetElementId);
-//     if (targetElement) {
-//       targetElement.innerHTML = ""; // Clear any existing content
-
-//       categories.forEach((category) => {
-//         const { _id, name, image, productCount } = category;
-//         const categoryObj = new Category(_id, name, image, productCount);
-//         const categoryHtml = `
-//           <div id="category-box-${categoryObj.id}" class="col-lg-3 col-md-4 col-sm-6 pb-1">
-//             <a class="text-decoration-none" href="products.php?cat_id=${categoryObj.id}">
-//               <div class="cat-item d-flex align-items-center mb-4">
-//                 <div class="overflow-hidden" style="width: 100px; height: 100px">
-//                   <img class="img-fluid" src="${categoryObj.image}" alt="${categoryObj.name}" />
-//                 </div>
-//                 <div class="flex-fill pl-3">
-//                   <h6>${categoryObj.name}</h6>
-//                   <small class="text-body">${categoryObj.productCount}</small>
-//                 </div>
-//               </div>
-//             </a>
-//           </div>`;
-//         targetElement.insertAdjacentHTML("beforeend", categoryHtml);
-//       });
-//     }
-//   }
-// }
-
-// // Usage
-// const categoryMenuRenderer = new CategoryMenuRenderer("http://localhost:5000/api/categories/", "categories-menu");
-// categoryMenuRenderer.init();
-
-// const categoryBoxRenderer = new CategoryBoxRenderer("http://localhost:5000/api/categories/", "categoriesboxs");
-// categoryBoxRenderer.init();
 
 
