9710c41fdb68a557a34cd806eea9a8dc19085968
Fouad Abdelhady <fouad.abd-elhady@outlook.com>
2023-06-08 19:40:52 +0200

    some basic changes

--- js/common/productItem.js
+++ js/common/productItem.js
@@ -119,7 +119,7 @@ class ProductItem{
     }
     addToCart(){
         console.log("in product item");
-       cart.addToCart(this);
+       cart.addToCartFromProductObj(this);
     }
     addToFavs(){
 
@@ -276,24 +276,3 @@ let response = [
         "__v": 0
     }
 ]
-function renderItems(){
-    let productsSection = document.getElementById("productsSection");
-    let product;
-    response.forEach((item)=>{
-        product = new ProductItem(item);
-        productsSection.appendChild(product.renderProduct());
-    });
-    let nav = document.createElement("div");
-    let content = `<nav>
-    <ul class="pagination justify-content-center">
-      <li class="page-item disabled"><a class="page-link" href="#">Previous</span></a></li>
-      <li class="page-item active"><a class="page-link" href="#">1</a></li>
-      <li class="page-item"><a class="page-link" href="#">2</a></li>
-      <li class="page-item"><a class="page-link" href="#">3</a></li>
-      <li class="page-item"><a class="page-link" href="#">Next</a></li>
-    </ul>
-  </nav>`
-    nav.innerHTML = content;
-    productsSection.appendChild(nav);
-}
-renderItems();
--- js/pages/shop.js
+++ js/pages/shop.js
@@ -1,20 +1,42 @@
-function renderItems(){
-    let productsSection = document.getElementById("productsSection");
-    let product;
-    response.forEach((item)=>{
-        product = new ProductItem(item);
-        productsSection.appendChild(product.renderProduct());
-    });
-    let nav = document.createElement("div");
-    let content = `<nav>
-    <ul class="pagination justify-content-center">
-      <li class="page-item disabled"><a class="page-link" href="#">Previous</span></a></li>
-      <li class="page-item active"><a class="page-link" href="#">1</a></li>
-      <li class="page-item"><a class="page-link" href="#">2</a></li>
-      <li class="page-item"><a class="page-link" href="#">3</a></li>
-      <li class="page-item"><a class="page-link" href="#">Next</a></li>
-    </ul>
-  </nav>`
-    nav.innerHTML = content;
-    productsSection.appendChild(nav);
+
+class ShopPageManager{
+    #current;
+    #next;
+    #previous;
+    #productsList;
+    constructor(){
+       
+    }
+
+    onLoad(){
+        let productsSection = document.getElementById("productsSection");
+        let product;
+        response.forEach((item)=>{
+            product = new ProductItem(item);
+            productsSection.appendChild(product.renderProduct());
+        });
+
+    }
+    renderItems(){
+        
+     
+    }
+    buildNameSection(){
+        let nav = document.getElementById('pagesBar');
+        let content = `<div class="col-12">
+        <nav>
+          <ul class="pagination justify-content-center">
+            <li class="page-item disabled"><a class="page-link" href="#">Previous</span></a></li>
+            <li class="page-item active"><a class="page-link" href="#">1</a></li>
+            <li class="page-item"><a class="page-link" href="#">2</a></li>
+            <li class="page-item"><a class="page-link" href="#">3</a></li>
+            <li class="page-item"><a class="page-link" href="#">Next</a></li>
+          </ul>
+        </nav>
+    </div>`
+        nav.innerHTML = content;
+    }
 }
+
+let shopPageManager = new ShopPageManager();
+shopPageManager.onLoad();
